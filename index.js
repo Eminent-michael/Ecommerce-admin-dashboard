9d8bcd710eaf2ed2a7549b89dc3aa77bdb230f2f
Eminent-michael <michaelafolab@gmail.com>
2023-06-08 20:29:09 +0100

    Added dark mode

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
+// Show sidebar
 menuBtn.addEventListener("click", () => {
-    console.log("Menu button clicked....")
     sideMenu.style.display = "block";
 })
 
+// Show sidebar
 closeBtn.addEventListener("click", () => {
     sideMenu.style.display = "none";
+})
+
+// Change theme
+themeToggler.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme-variables")
+
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 })
